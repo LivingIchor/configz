7a36d4081c57bfa88ca171c5928099a9cf18f872
LivingIchor <livingichor@gmail.com>
2026-03-17 20:17:36 -0400

    Refactor to use two hashmaps for wd to directory to statistics association

--- src/auto.zig
+++ src/auto.zig
@@ -2,24 +2,21 @@ const std = @import("std");
 const c = @import("c.zig").git2;
 const config = @import("config.zig");
 
-const FileGroup = struct {
-    dirname: ?[]const u8,
-    file_set: ?std.ArrayList([]const u8),
-};
-
-const WatchEntry = struct {
+const WatchDirStat = struct {
     is_explicit: bool,
-    file_group: FileGroup,
+    file_set: ?std.ArrayList([]const u8),
 };
 
 fn handleEvent(
     allocator: std.mem.Allocator,
     event: *std.os.linux.inotify_event,
-    wd_hash: *std.AutoHashMap(usize, WatchEntry)
+    wd_dirhash: *std.AutoHashMap(usize, []const u8),
+    dir_stathash: *std.StringHashMap(WatchDirStat)
 ) !void {
     _ = allocator;
     _ = event;
-    _ = wd_hash;
+    _ = wd_dirhash;
+    _ = dir_stathash;
 }
 
 pub fn watchFiles(init: std.process.Init, repo: *c.git_repository) !void {
@@ -39,60 +36,43 @@ pub fn watchFiles(init: std.process.Init, repo: *c.git_repository) !void {
     var out_wdirs = wdirs_reader.interface;
     // var in_wdirs = wdirs_writer.interface;
 
-    var expl_dirs = try getExplicitDirs(init.gpa, &out_wdirs);
-    defer expl_dirs.deinit(init.gpa);
-    defer for (expl_dirs.items) |d| init.gpa.free(d);
-
-    var file_dirs = try getTrackedFiles(init.gpa, repo);
-    defer file_dirs.deinit(init.gpa); // ArrayList(FileGroup)
-    defer for (file_dirs.items) |f| { // f = FileGroup
-        if (f.file_set) |set| {
-            var mset = set;
-            for (mset.items) |file| init.gpa.free(file);
-            mset.deinit(init.gpa);
-        }
-        if (f.dirname) |dir| init.gpa.free(dir);
-    };
+    var expl_stats = try getExplicitDirs(init.gpa, &out_wdirs);
+    var file_stats = try getTrackedFiles(init.gpa, repo);
+    var it = expl_stats.iterator();
+    while (it.next()) |entry| {
+        try file_stats.put(entry.key_ptr.*, entry.value_ptr.*);
+    }
+    expl_stats.deinit();
+    defer freeEntries(init.gpa, &file_stats);
 
     // Initialize inotify
     const ifd: i32 = @intCast(std.os.linux.inotify_init1(std.os.linux.IN.NONBLOCK));
 
-    var wd_files = std.AutoHashMap(usize, WatchEntry).init(init.gpa);
-    defer wd_files.deinit();
+    var wd_dirs = std.AutoHashMap(usize, []const u8).init(init.gpa);
+    defer wd_dirs.deinit();
 
     // Add watches
     var wd: usize = undefined;
-    for (expl_dirs.items) |dirpath| {
-        wd = std.os.linux.inotify_add_watch(ifd, repo_path,
+
+    // Add watch for the repo itself
+    wd = std.os.linux.inotify_add_watch(ifd, repo_path,
+        std.os.linux.IN.MODIFY | std.os.linux.IN.CREATE |
+        std.os.linux.IN.DELETE | std.os.linux.IN.MOVED_FROM |
+        std.os.linux.IN.MOVED_TO);
+    try wd_dirs.put(wd, std.mem.span(repo_path));
+
+    // Add watches for all other tracked directories and files
+    var statit = file_stats.iterator();
+    while (statit.next()) |stat| {
+        const abs_path = try std.fmt.allocPrint(init.gpa, "{s}/{s}", .{home, stat.key_ptr.*});
+        defer init.gpa.free(abs_path);
+
+        wd = std.os.linux.inotify_add_watch(ifd, @ptrCast(&abs_path),
             std.os.linux.IN.MODIFY | std.os.linux.IN.CREATE |
             std.os.linux.IN.DELETE | std.os.linux.IN.MOVED_FROM |
             std.os.linux.IN.MOVED_TO);
 
-        const we = try init.gpa.create(WatchEntry);
-        we.* = WatchEntry{
-            .is_explicit = true,
-            .file_group = FileGroup{
-                .dirname = dirpath,
-                .file_set = null,
-            },
-        };
-
-        try wd_files.put(wd, we.*);
-    }
-
-    for (file_dirs.items) |group| {
-        wd = std.os.linux.inotify_add_watch(ifd, repo_path,
-            std.os.linux.IN.MODIFY | std.os.linux.IN.CREATE |
-            std.os.linux.IN.DELETE | std.os.linux.IN.MOVED_FROM |
-            std.os.linux.IN.MOVED_TO);
-
-        const we = try init.gpa.create(WatchEntry);
-        we.* = WatchEntry{
-            .is_explicit = false,
-            .file_group = group,
-        };
-
-        try wd_files.put(wd, we.*);
+        try wd_dirs.put(wd, stat.key_ptr.*);
     }
 
     var buf: [4096]u8 align(@alignOf(std.os.linux.inotify_event)) = undefined;
@@ -105,7 +85,7 @@ pub fn watchFiles(init: std.process.Init, repo: *c.git_repository) !void {
         while (offset < len) {
             const event: *std.os.linux.inotify_event = @ptrCast(@alignCast(&buf[offset]));
 
-            try handleEvent(init.gpa, event, &wd_files);
+            try handleEvent(init.gpa, event, &wd_dirs, &file_stats);
 
             offset += @sizeOf(std.os.linux.inotify_event) + event.len;
         }
@@ -115,23 +95,28 @@ pub fn watchFiles(init: std.process.Init, repo: *c.git_repository) !void {
 fn getExplicitDirs(
     allocator: std.mem.Allocator,
     manifest_reader: *std.Io.Reader
-) !std.ArrayList([]const u8) {
-    var dirs = try std.ArrayList([]const u8).initCapacity(allocator, 0);
+) !std.StringHashMap(WatchDirStat) {
+    var dir_stats = std.StringHashMap(WatchDirStat).init(allocator);
 
     while (manifest_reader.takeSentinel('\n') catch |err| switch (err) {
         error.EndOfStream => null,
         else => return err,
     }) |dir| {
-        try dirs.append(allocator, try allocator.dupe(u8, dir));
+        const dirname = try allocator.dupe(u8, dir);
+        const stat_entry = WatchDirStat{
+            .is_explicit = true,
+            .file_set = null,
+        };
+        try dir_stats.put(dirname, stat_entry);
     }
 
-    return dirs;
+    return dir_stats;
 }
 
 fn getTrackedFiles(
     allocator: std.mem.Allocator,
     repo: *c.git_repository
-) !std.ArrayList(FileGroup) {
+) !std.StringHashMap(WatchDirStat) {
     var index: ?*c.git_index = null;
     if (c.git_repository_index(&index, repo) != 0) return error.GitError;
     defer c.git_index_free(index);
@@ -140,48 +125,62 @@ fn getTrackedFiles(
     _ = c.git_index_read(index, 1);
 
     const count = c.git_index_entrycount(index);
-    var dirs = try std.ArrayList(FileGroup).initCapacity(allocator, 0);
+    var dir_stats = std.StringHashMap(WatchDirStat).init(allocator);
 
-    var entry = c.git_index_get_byindex(index, 0);
-    var current_dirname: ?[]const u8 = std.fs.path.dirname(std.mem.span(entry.*.path));
+    var index_entry = c.git_index_get_byindex(index, 0);
+    var current_dirname: []const u8 = std.fs.path.dirname(std.mem.span(index_entry.*.path)) orelse "";
     var file_set = try std.ArrayList([]const u8).initCapacity(allocator, 0);
     try file_set.append(allocator, try allocator.dupe(
-        u8, std.fs.path.basename(std.mem.span(entry.*.path))));
+        u8, std.fs.path.basename(std.mem.span(index_entry.*.path))));
 
-    var dir_group = try allocator.create(FileGroup);
-    dir_group.* = FileGroup{
-        .dirname = if (current_dirname) |dir| try allocator.dupe(u8, dir) else null,
+    var stat_entry = WatchDirStat{
+        .is_explicit = false,
         .file_set = file_set,
     };
 
-    try dirs.append(allocator, dir_group.*);
+    try dir_stats.put(current_dirname, stat_entry);
 
     var i: usize = 1;
     while (i < count) : (i += 1) {
-        entry = c.git_index_get_byindex(index, i);
-        const new_dirname: ?[]const u8 = std.fs.path.dirname(std.mem.span(entry.*.path));
+        index_entry = c.git_index_get_byindex(index, i);
+        const new_dirname: []const u8 = std.fs.path.dirname(std.mem.span(index_entry.*.path)) orelse "";
 
-        if ((current_dirname == null and new_dirname == null)
-            or ((current_dirname != null and new_dirname != null)
-            and std.mem.eql(u8, current_dirname.?, new_dirname.?))
-        ) {
+        if (std.mem.eql(u8, current_dirname, new_dirname)) {
             try file_set.append(allocator, try allocator.dupe(
-                u8, std.fs.path.basename(std.mem.span(entry.*.path))));
+                u8, std.fs.path.basename(std.mem.span(index_entry.*.path))));
         } else {
             current_dirname = new_dirname;
             file_set = try std.ArrayList([]const u8).initCapacity(allocator, 0);
             try file_set.append(allocator, try allocator.dupe(
-                u8, std.fs.path.basename(std.mem.span(entry.*.path))));
+                u8, std.fs.path.basename(std.mem.span(index_entry.*.path))));
 
-            dir_group = try allocator.create(FileGroup);
-            dir_group.* = FileGroup{
-                .dirname = if (current_dirname) |dir| try allocator.dupe(u8, dir) else null,
+            stat_entry = WatchDirStat{
+                .is_explicit = false,
                 .file_set = file_set,
             };
 
-            try dirs.append(allocator, dir_group.*);
+            try dir_stats.put(current_dirname, stat_entry);
         }
     }
 
-    return dirs;
+    return dir_stats;
+}
+
+fn freeEntries(allocator: std.mem.Allocator, entries: *std.StringHashMap(WatchDirStat)) void {
+    defer entries.deinit();
+    var it = entries.iterator();
+    while (it.next()) |entry| {
+        const dirname = entry.key_ptr.*;
+        var watch_dir_stat = entry.value_ptr.*;
+
+        allocator.free(dirname);
+
+        if (watch_dir_stat.file_set) |set| {
+            var mset = set;
+            for (set.items) |dir| {
+                allocator.free(dir);
+            }
+            mset.deinit(allocator);
+        }
+    }
 }
